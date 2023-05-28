7260b79ec3addb78aa5fa4b1f1b32be81f07e139
itom-qe <79703359+itom-qe@users.noreply.github.com>
2023-05-29 01:29:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
+  version = "4.0.2"
 
   name                 = "test"
   cidr                 = "10.0.0.0/16"
