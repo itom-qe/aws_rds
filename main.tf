e1839309d3ecf57fd066b7acd828aca232114cce
itom-qe <79703359+itom-qe@users.noreply.github.com>
2023-05-29 01:20:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,6 +24,8 @@ module "vpc" {
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_dns_hostnames = true
   enable_dns_support   = true
+  enable_classiclink   = false
+  enable_classiclink_dns_support = false
 }
 
 
